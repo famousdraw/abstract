9c0c7405597533cf624234b00e21d17b475eea09
Jason Zhao <famousdraw@163.com>
2020-03-16 23:22:51 +0800

    add knn treat

--- examples/inference.py
+++ examples/inference.py
@@ -77,8 +77,87 @@ def inference_from_embs(embs,sen):
     for i,value in result_rel:
         output.append(sentences[i])
     print(output)
+    return rel
+
+def knn_treat(rel):
+    print(__doc__)
+    rel=rel
+    # Author: Alexandre Gramfort <alexandre.gramfort@inria.fr>
+    #         Fabian Pedregosa <fabian.pedregosa@inria.fr>
+    #
+    # License: BSD 3 clause (C) INRIA
 
 
+    # #############################################################################
+    # Generate sample data
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from sklearn import neighbors
 
+    np.random.seed(0)
+    #X = np.sort(5 * np.random.rand(40, 1), axis=0)
+
+    #T = np.linspace(0, 5, 500)[:, np.newaxis]
+    #y = np.sin(X).ravel()
+
+
+    # Add noise to targets
+    #y[::5] += 1 * (0.5 - np.random.rand(8))
+
+    X=[]
+    y=[]
+    for i,j in rel:
+        X.append([i])
+        y.append([j])
+    T=X
+
+    # #############################################################################
+    # Fit regression model
+    n_neighbors = 3
+
+    #for i, weights in enumerate(['uniform', 'distance']):
+    for i, weights in enumerate(['uniform']):
+        knn = neighbors.KNeighborsRegressor(n_neighbors, weights=weights)
+        y_ = knn.fit(X, y).predict(T)
+
+        #plt.subplot(2, 1, i + 1)
+        #plt.scatter(X, y, color='darkorange', label='data')
+        #plt.plot(T, y_, color='navy', label='prediction')
+        #plt.axis('tight')
+        #plt.legend()
+        #plt.title("KNeighborsRegressor (k = %i, weights = '%s')" % (n_neighbors,
+        #                                                            weights))
+
+    #plt.tight_layout()
+    #plt.show()
+    return y_
+
+def inference_after_knn(y_,sen):
+    y_=y_
+    y_reshape=y_.reshape(-1,)
+    sentences=sen
+    from operator import itemgetter, attrgetter
+    rel=[]
+    rel_sorted=[]
+    for i in range(len(sentences)-1):
+        #print(i)
+        rel.append( (i,y_reshape[i]))
+    rel_sorted=sorted(rel, key=itemgetter(1), reverse=True)
+    k=0
+    #print(rel_sorted)
+    result_rel=[]
+    for i in range(len(rel_sorted)):
+        if k==40:break
+        k+=1
+        result_rel.append(rel_sorted[i])
+
+    k==0
+    result_rel.sort(key=itemgetter(0))
+    #print(result_rel)   
+    output=[]
+    print(sentences[:-2])
+    for i,value in result_rel:
+        output.append(sentences[i])
+    print(output)
 
 
--- examples/test.py
+++ examples/test.py
@@ -2,4 +2,7 @@ import sys
 import inference
 
 embs,sen=inference.prepare_embs()
-inference.inference_from_embs(embs,sen)
+rel=inference.inference_from_embs(embs,sen)
+y_=inference.knn_treat(rel)
+inference.inference_after_knn(y_,sen)
+
